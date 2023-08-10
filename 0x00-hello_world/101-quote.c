985b78523b3fe9969d8a484198539bd8a28742af
bajaroali <bajaroali@gmail.com>
2023-08-10 02:36:20 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - prints to the standard error
+ *
+ * Return: success return zero
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
