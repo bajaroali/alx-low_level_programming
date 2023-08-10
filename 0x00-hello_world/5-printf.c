fe119f6087a0afde19788c3af0015878fc3ecdad
bajaroali <bajaroali@gmail.com>
2023-08-10 01:57:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main prints with proper grammar, but the outcome is ....
+ *
+ *On success return zero
+ *
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
