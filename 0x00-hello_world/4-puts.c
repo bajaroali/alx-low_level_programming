70d396825a13653a8e9e98af721188df547b8442
bajaroali <bajaroali@gmail.com>
2023-08-10 01:50:29 -0700

    4-puts.c amended

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Prints Programming is like building a....
  *
- * the main function
+ * Return: zero on success
+ *
  */
-
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
