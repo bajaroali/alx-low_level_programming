70d3c868264f60973c57cb23dd19b6205f1c9c75
bajaroali <bajaroali@gmail.com>
2023-08-14 23:37:05 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs -to  compute absolute value of n
+ * @n: int to compute as absolute
+ * Return: absolute value from _abs
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
