280699f176928e76e6a847b412d01e5de173074c
bajaroali <bajaroali@gmail.com>
2023-08-21 01:58:38 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print  a string in reverse, followed by a\n
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int j, k, len;
+
+	j = 0;
+
+	while (s[j] != '\0')
+	{
+		i++;
+	}
+
+	len = j;
+
+	for (k = len - 1; k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+
+	_putchar('\n');
+}
