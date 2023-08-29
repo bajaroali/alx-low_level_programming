879993976fcef349dd3f9b8985738b596a59200a
bajaroali <64841706+bajaroali@users.noreply.github.com>
2023-08-29 09:36:23 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * print_diagsums - prints a sum of 2 diagonals
+ * of a SQ Matrix of integers
+ * @a: square Matrix which will print the sum of diagonals
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum += a[(size * i) + i];
+		sum1 += a[(size * (i + 1)) - (i + 1)];
+	}
+
+	printf("%d, %d\n", sum, sum1);
+}
